c942362042382c2f510a0ae08272745673662e53
Konstantinos Lampropoulos <kostas@KonstaninossAir.home>
2023-08-05 17:49:11 +0300

    Implement first test.

--- bdd-example/src/test/java/com/pluralsight/bdd/CalculatorStepDefinitions.java
+++ bdd-example/src/test/java/com/pluralsight/bdd/CalculatorStepDefinitions.java
@@ -6,7 +6,13 @@ import io.cucumber.java.en.When;
 
 import java.util.Random;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class CalculatorStepDefinitions {
+
+    private Calculator calculator;
+    private int result;
 
     public CalculatorStepDefinitions() {
         System.out.println("Constructor Called again , " + new Random());
@@ -14,14 +20,16 @@ public class CalculatorStepDefinitions {
 
     @Given("I have a calculator")
     public void i_have_a_calculator() {
+        calculator = new Calculator();
 
     }
     @When("I add {int} and {int}")
     public void i_add_and(Integer int1, Integer int2) {
+        result = calculator.add(int1, int2);
 
     }
     @Then("I should get {int}")
-    public void i_should_get(Integer int1) {
-
+    public void i_should_get(Integer expectedResult) {
+        assertEquals(result, (int) expectedResult);
     }
 }
